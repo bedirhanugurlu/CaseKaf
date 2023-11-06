79526d9bd89606a812af3a17ce6c8081a46a52cc
Bedirhan Uğurlu <bedirhanugurlu0@gmail.com>
2023-11-06 10:55:48 +0300

    first commit

---
+++ assets/javascript/theme.js
@@ -0,0 +1,45 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+    tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+        tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
+
+$(document).ready(function() {
+    $('input[type="file"]').on("change", function() {
+      let filenames = [];
+      let files = this.files;
+      if (files.length > 1) {
+        filenames.push("Total Files (" + files.length + ")");
+      } else {
+        for (let i in files) {
+          if (files.hasOwnProperty(i)) {
+            filenames.push(files[i].name);
+          }
+        }
+      }
+      $(this)
+        .next(".custom-file-label")
+        .html(filenames.join(","));
+    });
+});
